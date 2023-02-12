b792716ccdcd33f80fa66e47919bcfbb2f7f3cfb
DanGrutsyuk <Dan_Gritsyuk>
2023-02-12 14:09:30 +0300

    Add exercise 15 and upgrade InputNumbers class

--- Exercise10.cs
+++ Exercise10.cs
@@ -1,3 +1,5 @@
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
 namespace Lesson1
 {
     public class Exercise10 : Exercise
@@ -7,27 +9,9 @@ namespace Lesson1
         public override void Body()
         {
             Console.Write("Введите число (n): ");
-            int n = GetThreedigitNumberFromConsole();
+            int n = InputNumbers.GetNumberInRangeFromConsole(100, 999, "Веедено не трехзначное число. Введите корректное: ");
 
             Console.WriteLine($"Вторая цифра числа {n} это {n / 10 % 10}");
-        }
-
-        private int GetThreedigitNumberFromConsole()
-        {
-            int n = 0;
-            Func<bool> tryGetThreedigitFromConsole = () => // Функция для вызова одного и того же кода в нескольких местах в этом методе 
-            {
-                n = InputNumbers.GetNumberFromConsole();
-                return n > 99 && n < 1000;
-            };
-
-            bool nCorrect = tryGetThreedigitFromConsole();
-            while (!nCorrect)
-            {
-                Console.Write("Веедено не трехзначное число. Введите корректное: ");
-                nCorrect = tryGetThreedigitFromConsole();
-            }
-            return n;
         }
     }
 }
--- InputNumbers.cs
+++ InputNumbers.cs
@@ -9,5 +9,23 @@ namespace Lesson1
                 Console.Write("Введено некоректное число. Повторите попытку: ");
             return number;
         }
+
+        public static int GetNumberInRangeFromConsole(int first, int last, string errorMessage)
+        {
+            int n = 0;
+            Func<bool> tryGetThreedigitFromConsole = () => // Функция для вызова одного и того же кода в нескольких местах в этом методе 
+            {
+                n = InputNumbers.GetNumberFromConsole();
+                return n >= first && n <= last;
+            };
+
+            bool nCorrect = tryGetThreedigitFromConsole();
+            while (!nCorrect)
+            {
+                Console.Write(errorMessage);
+                nCorrect = tryGetThreedigitFromConsole();
+            }
+            return n;
+        }
     }
 }
