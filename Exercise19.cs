32d5db349086e8e1cbdab5550482ca6f78925227
DanGrutsyuk <Dan_Gritsyuk>
2023-02-18 18:33:15 +0300

    Add exercise 19 file

---
+++ Exercise19.cs
@@ -0,0 +1,24 @@
+// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+namespace Lessons
+{
+    public class Exercise19 : Exercise
+    {
+        public Exercise19(int taskNum) : base(taskNum) { }
+
+        public override void Body()
+        {
+            Console.Write("Введите пятизначное число: ");
+            int copyNumber, number = copyNumber = InputNumbers.GetNumberFromConsole(10000, 99999, "Введено не пятизначное число. Повторите попытку: ");
+
+            int reverceNumber = 0;
+            while (number > 0)
+            {
+                reverceNumber = reverceNumber * 10 + copyNumber % 10;
+                copyNumber /= 10;
+            }
+
+            Console.WriteLine($"Число {number} {(number == reverceNumber ? "палиндром" : "не палиндром")}");
+        }
+    }
+}
