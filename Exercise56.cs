143b0dbe4add60edae7d82beaae9bab1813ccff1
DanGrutsyuk <Dan_Gritsyuk>
2023-03-05 16:05:10 +0300

    Exercise 58 done

--- Exercise56.cs
+++ Exercise56.cs
@@ -1,4 +1,4 @@
-//
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
 namespace Lessons
 {
@@ -7,7 +7,27 @@ namespace Lessons
         public Exercise56(KeyValuePair<int, string> taskData) : base(taskData) { }
         public override void Body()
         {
-            throw new NotImplementedException();
+            int[,] array = InputNumbers.GetTwoDimensionalArrayFromConsole<int>();
+
+            int lineIndex = 0;
+            int? sumMin = null;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < array.GetLength(1); j++)
+                    sum += array[i, j];
+                if (sumMin == null)
+                {
+                    sumMin = sum;
+                }
+                else if (sum < sumMin.Value)
+                {
+                    sumMin = sum;
+                    lineIndex = i;
+                }
+            }
+
+            Console.WriteLine($"{lineIndex + 1}-я строка с наименьшей суммой элементов.");
         }
     }
 }
--- Exercise58.cs
+++ Exercise58.cs
@@ -1,4 +1,4 @@
-//
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
 namespace Lessons
 {
@@ -7,7 +7,32 @@ namespace Lessons
         public Exercise58(KeyValuePair<int, string> taskData) : base(taskData) { }
         public override void Body()
         {
-            throw new NotImplementedException();
+            int[,] matrixA = InputNumbers.GetTwoDimensionalArrayFromConsole<int>();
+            Console.WriteLine();
+            int[,] matrixB = InputNumbers.GetTwoDimensionalArrayFromConsole<int>();
+
+            if (ColumnsCount(matrixA) != RowsCount(matrixB))
+            {
+                Console.WriteLine("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+                return;
+            }
+
+            var matrixC = new int[RowsCount(matrixA), ColumnsCount(matrixB)];
+
+            for (var i = 0; i < RowsCount(matrixA); i++)
+            {
+                for (var j = 0; j < ColumnsCount(matrixB); j++)
+                {
+                    matrixC[i, j] = 0;
+                    for (var k = 0; k < ColumnsCount(matrixA); k++)
+                        matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                }
+            }
+
+            PrintObjects.PrintArray(matrixC);
         }
+
+        private static int RowsCount(int[,] matrix) => matrix.GetUpperBound(0) + 1;
+        private static int ColumnsCount(int[,] matrix) => matrix.GetUpperBound(1) + 1;
     }
 }
