e520088889baaad737eed07af8f691c09ab9477d
DanGrutsyuk <Dan_Gritsyuk>
2023-03-26 08:57:12 +0300

    Add new class FinalProject

---
+++ FinalProject/FinalProject.cs
@@ -0,0 +1,14 @@
+// Итоговый проект. Напишите программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+namespace Lessons
+{
+    public class FinalProject : Exercise
+    {
+        public FinalProject(KeyValuePair<int, string> taskData) : base(taskData) { }
+
+        public override void Body()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
